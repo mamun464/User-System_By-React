fdc900a07cf86ac1bbad7348f063d12ad8355136
Mamun <mamunurrashid.s.bd@gmail.com>
2023-12-19 23:28:53 +0600

    Deploy push

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -2,7 +2,7 @@
 const ErrorPage = () => {
     return (
         <div className="">
-            <img className="w-screen h-screen" src="../../../public/404page.png" alt="Not_found" />
+            <img className="w-screen h-screen" src="https://i.ibb.co/SnW7GC7/404page.png" alt="Not_found" />
         </div>
     );
 };
